ecf8e62334adc2aed7e3ffd081470152a1a7bc0e
Ryan Long <rlong@redhat.com>
2022-12-11 16:32:34 -0500

    add uptime (#3)

---
+++ commands/prefix/Info/uptime.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder } = require("discord.js"); 
+
+module.exports = {
+  config: {
+    name: "uptime",
+    description: "Replies with bot uptime!",
+  },
+  permissions: ['SendMessages'],
+  owner: false,
+  run: async (client, message, args, prefix, config, db) => {
+
+    message.reply({ embeds: [
+      new EmbedBuilder()
+        .setDescription(`bot has been up for ${new Date(process.uptime() * 1000).toISOString().slice(11, 19)}s`)
+        .setColor("Green")
+    ] })
+    
+  },
+};
