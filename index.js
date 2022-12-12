c063ff1cc89c885331d7f63aad5324232af3df58
Ryan Long <rlong@redhat.com>
2022-12-12 12:44:55 -0500

    fix index file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { REST, Routes, Client, Partials, Collection, GatewayIntentBits } = require('discord.js');
+const {
+  REST, Routes, Client, Partials, Collection, GatewayIntentBits,
+} = require('discord.js');
+const colors = require('colors');
 const config = require('./config/config');
 const guild = require('./config/guild');
-const colors = require("colors");
 require('dotenv').config();
 
-const TOKEN = process.env.TOKEN;
+const { TOKEN } = process.env;
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
@@ -14,25 +16,25 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildPresences,
-		GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
   ],
   partials: [
     Partials.Channel,
     Partials.Message,
     Partials.User,
     Partials.GuildMember,
-    Partials.Reaction
+    Partials.Reaction,
   ],
   presence: {
     activities: [{
-      name: "VALORANT, probably",
-      type: 0
+      name: 'VALORANT, probably',
+      type: 0,
     }],
-    status: 'online'
-  }
+    status: 'online',
+  },
 });
 
 // Host the bot:
@@ -40,9 +42,9 @@ require('http').createServer((req, res) => res.end('Ready.')).listen(3000);
 
 // Getting the bot token:
 if (!TOKEN) {
-  console.warn("[CRASH] Authentication Token for Discord bot is required! Use Envrionment Secrets.".red)
-  return process.exit();
-};
+  console.warn('[CRASH] Authentication Token for Discord bot is required! Use Envrionment Secrets.'.red);
+  process.exit();
+}
 
 // Handler:
 client.prefix_commands = new Collection();
@@ -54,15 +56,15 @@ client.events = new Collection();
 
 module.exports = client;
 
-["prefix", "application_commands", "modals", "events", "mongoose"].forEach((file) => {
+['prefix', 'application_commands', 'modals', 'events', 'mongoose'].forEach((file) => {
   require(`./handlers/${file}`)(client, config);
 });
 
 // Login to the bot:
 client.login(TOKEN)
   .catch((err) => {
-    console.error("[CRASH] Something went wrong while connecting to your bot...");
-    console.error("[CRASH] Error from Discord API:" + err);
+    console.error('[CRASH] Something went wrong while connecting to your bot...');
+    console.error(`[CRASH] Error from Discord API:${err}`);
     return process.exit();
   });
 
@@ -74,5 +76,5 @@ process.on('unhandledRejection', async (err, promise) => {
 
 // for global commands
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
+  .then(() => console.log('Successfully deleted all guild commands.'))
+  .catch(console.error);
