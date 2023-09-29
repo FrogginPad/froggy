f92ab57c08393dc4cf1f65fe6fafc4d8481093ea
Austin Pinkerton <115492521+apinkert@users.noreply.github.com>
2023-09-29 10:45:03 -0400

    add clearmatches and getmatches commands (#18)
    * add clearmatches and getmatches commands
    
    * appease linter

---
+++ commands/slash/General/clearmatches.js
@@ -0,0 +1,22 @@
+const guild = require('../../../config/guild');
+
+module.exports = {
+  name: 'clearmatches',
+  description: 'Clears the matches channel',
+  type: 1,
+  options: [],
+  permissions: {
+    DEFAULT_MEMBER_PERMISSIONS: 'SendMessages',
+  },
+  run: async (client, interaction, config, db) => {
+    const channel = client.channels.cache.get(guild.Channels.OnlyFrogs.matchesText);
+    const messageManager = channel.messages;
+    const messages = await messageManager.channel.messages.fetch({ limit: 100 });
+    channel.bulkDelete(messages, true);
+
+    return interaction.reply({
+      content: 'Cleared!',
+      ephemeral: true,
+    });
+  },
+};
